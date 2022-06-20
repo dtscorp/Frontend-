b784406e7f3ecb5a66feb0df9462a18d6ba16ec5
Danang TS <danangtskhair07islm@gmail.com>
2022-06-20 21:22:38 +0700

    fixing GH pages

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* Bungkus app dengan BrowserRouter  */}
-    <BrowserRouter>
+    {/* Bungkus app dengan HashRouter  */}
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
